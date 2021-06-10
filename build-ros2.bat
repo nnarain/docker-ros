6611a55748cd5e5e9cb2d7ff0ae2acddfcd8866f
Natesh Narain <nnaraindev@gmail.com>
2021-06-09 21:28:35 -0400

    foxy...

--- build-ros2.bat
+++ build-ros2.bat
@@ -1,3 +1,8 @@
 docker buildx build --platform linux/amd64,linux/arm64 -f ros2\base\Dockerfile    -t nnarain/ros2-base:latest    --no-cache --output type=registry .
+docker pull nnarain/ros2-base:latest
+
 docker buildx build --platform linux/amd64             -f ros2\desktop\Dockerfile -t nnarain/ros2-desktop:latest --no-cache --output type=registry .
+docker pull nnarain/ros2-desktop:latest
+
 docker buildx build --platform linux/amd64             -f ros2\dev\Dockerfile     -t nnarain/ros2-dev:latest     --no-cache --output type=registry .
+docker pull nnarain/ros2-dev:latest
